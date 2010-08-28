c37d1858dc30df3a276594713c47228f836f426a
Kyle Simpson <getify@gmail.com>
2010-08-28 18:20:27 -0500

    more drag-n-drop piece logic, overlap detection, etc

--- public/js/gameboard.js
+++ public/js/gameboard.js
@@ -2,8 +2,80 @@
 
 	var $board = $("<div></div>"),
 		row_names = "ABCDEFGHIJKLMNO".split(""),
-		$row, $cell, $target_board, $my_board
+		$row, $cell, $piece_position_marker, ppm_pos, my_board_offset
 	;
+	
+	function piece_conflicts() {
+		var $pieces = global.$my_board.find(".piece"), conflict_found = false;
+		
+		$pieces.each(function(){
+			var $piece = $(this), p_pos = $piece.position();
+			p_pos.right = p_pos.left + $piece.width() - 3;
+			p_pos.bottom = p_pos.top + $piece.height() - 1;
+			if (!(
+				(ppm_pos.right < p_pos.left) || (ppm_pos.left > p_pos.right) || (ppm_pos.bottom < p_pos.top) || (ppm_pos.top > p_pos.bottom)
+			)) { conflict_found = true; return false; }
+		});
+		return conflict_found;
+	}
+	
+	function find_valid_piece_location($piece) {
+		var piece_off = $piece.offset(),
+			pw = $piece.width()-2, ph = $piece.height(),
+			pcw = 1 + Math.floor((pw-30)/32), pch = 1 + Math.floor((ph-30)/32),
+			px1 = piece_off.left-my_board_offset.left, py1 = piece_off.top-my_board_offset.top,
+			px2 = px1 + pw - 1, py2 = py1 + ph -1,
+			position = {
+				c1: Math.round(px1 / 32),
+				c2: Math.round(px2 / 32),
+				r1: Math.round(py1 / 32),
+				r2: Math.round(py2 / 32)
+			}
+		;
+		
+		// constrain piece_position_marker to board
+		if (position.c1 < 1) {
+			position.c1 = 1;
+			position.c2 = position.c1 + pcw - 1;
+		}
+		else if (position.c2 > 15) {
+			position.c2 = 15;
+			position.c1 = position.c2 - pcw + 1;
+		}
+		else {
+			position.c2 = position.c1 + pcw - 1;
+		}
+		
+		if (position.r1 < 1) {
+			position.r1 = 1;
+			position.r2 = position.r1 + pch - 1;
+		}
+		else if (position.r2 > 15) {
+			position.r2 = 15;
+			position.r1 = position.r2 - pch + 1;
+		}
+		else {
+			position.r2 = position.r1 + pch - 1;
+		}
+		
+		position.x1 = position.c1 * 32 + 2;
+		position.y1 = position.r1 * 32 + 2;
+		position.x2 = (position.c2+1) * 32 + 1;
+		position.y2 = (position.r2+1) * 32 + 1;
+		
+		ppm_pos = {left:position.x1, top:position.y1, right:position.x2, bottom:position.y2};
+		
+		if (!piece_conflicts() && Math.abs(px1-position.x1) <= 50 && Math.abs(px2-position.x2) <= 50 && Math.abs(py1-position.y1) <= 50 && Math.abs(py2-position.y2) <= 50) {
+			$piece_position_marker.css({left:position.x1+"px",top:position.y1+"px",width:(position.x2-position.x1-1)+"px",height:(position.y2-position.y1-1)+"px"}).show();
+			return position;
+		}
+		$piece_position_marker.hide();
+		return false;
+	}
+	
+	function hide_piece_location_marker() {
+		$piece_position_marker.hide();
+	}
 	
 	row_names.unshift("");
 	
@@ -28,13 +100,19 @@
 		
 		var $contents = $board.children("#headers, .row");
 		
-		$target_board = $("#target_board");
-		$target_board.append($contents.clone());
+		global.$target_board = $("#target_board");
+		global.$target_board.append($contents.clone());
 		
-		$my_board = $("#my_board");
-		$my_board.append($contents.clone());
+		global.$my_board = $("#my_board");
+		global.$my_board.append($contents.clone());
+		global.$my_board.find_valid_piece_location = find_valid_piece_location;
+		global.$my_board.hide_piece_location_marker = hide_piece_location_marker;
 		
-		$target_board.click(function(e){
+		$piece_position_marker = $("<div></div>").attr("id","piece_position_marker");
+		$piece_position_marker.appendTo(global.$my_board);
+		my_board_offset = global.$my_board.offset();
+		
+		global.$target_board.click(function(e){
 			var $et = $(e.target);
 			if ($et.is("a") && !$et.hasClass("hit")) {
 				$et.addClass("hit");
--- public/js/pieces.js
+++ public/js/pieces.js
@@ -3,8 +3,6 @@
 	var dragging = false,
 		valid_position = false,
 		$drag_piece,
-		$my_board,
-		$pieces,
 		orig_pos,
 		mouseX, mouseY,
 		piece_offset_x, piece_offset_y
@@ -22,28 +20,36 @@
 		piece_offset_y = tmp;
 		
 		$drag_piece.css({left:mouseX-piece_offset_x+"px",top:mouseY-piece_offset_y+"px"});
+		
+		valid_position = global.$my_board.find_valid_piece_location($drag_piece);
 	}
 	
 	function start_piece_drag(e) {
 		dragging = true;
 		
         // prevent image drag and text selection
-        document.onselectstart = e.target.ondragstart = function(e){e=e||window.event; e.preventDefault&&e.preventDefault(); e.returnValue = false; return false;};
+        document.onselectstart = this.ondragstart = function(e){e=e||window.event; e.preventDefault&&e.preventDefault(); e.returnValue = false; return false;};
 		
-		$drag_piece = $(e.target);
+		$drag_piece = $(this);
 		orig_pos = $drag_piece.position();
 		
-		var pieces_offset = $pieces.offset();
+		var parent_offset = $drag_piece.parents("#pieces_bin, #my_board").offset();
 		
-		mouseX = e.pageX - pieces_offset.left; 
-		mouseY = e.pageY - pieces_offset.top;
+		mouseX = e.pageX - parent_offset.left; 
+		mouseY = e.pageY - parent_offset.top;
 		piece_offset_x = mouseX - orig_pos.left;
 		piece_offset_y = mouseY - orig_pos.top;
 		
+		if ($drag_piece.parents("#my_board").length>0) {
+			orig_pos = {top:0};
+			if ($drag_piece.is("#titanic")) orig_pos.left = 0;
+			else if ($drag_piece.is("#warship")) orig_pos.left = 100;
+			else if ($drag_piece.is("#submarine")) orig_pos.left = 200;
+			else if ($drag_piece.is("#pirateship")) orig_pos.left = 300;
+			else if ($drag_piece.is("#tugboat")) orig_pos.left = 400;
+			else if ($drag_piece.is("#liferaft")) orig_pos.left = 480;
+		}
 		$drag_piece.appendTo("body");
-		
-		
-		$(document).bind("dragstart",function(e){e.preventDefault(); e.returnValue = false; return false;});
 		
 		$(document).bind("mousemove",drag_piece).bind("mouseup",stop_drag_piece).bind("keypress",function(e){
 			if (e.which == 82 || e.which == 114) { // `r` or `R`
@@ -61,6 +67,7 @@
 			mouseX = e.pageX;
 			mouseY = e.pageY;
 			$drag_piece.css({left:mouseX-piece_offset_x+"px",top:mouseY-piece_offset_y+"px"});
+			valid_position = global.$my_board.find_valid_piece_location($drag_piece);
 		}
 		else stop_drag_piece(e);
 	}
@@ -68,11 +75,13 @@
 	function stop_drag_piece(e) {
 		$(document).unbind("mousemove mouseup keypress");
 		
+		global.$my_board.hide_piece_location_marker();
 		if (valid_position) {
-			$drag_piece.appendTo($my_board);
+			$drag_piece.appendTo(global.$my_board);
+			$drag_piece.css({left:valid_position.x1+"px",top:valid_position.y1+"px"});
 		}
 		else {
-			$drag_piece.appendTo($pieces);
+			$drag_piece.appendTo(global.$pieces_bin);
 			$drag_piece.removeClass("horz").addClass("vert");
 			$drag_piece.css({left:orig_pos.left+"px",top:orig_pos.top+"px"});
 		}
@@ -84,8 +93,7 @@
 	}
 		  
 	$(document).ready(function(){
-		$my_board = $("#myboard");
-		$pieces = $("#pieces");
+		global.$pieces_bin = $("#pieces_bin");
 		
 		$("#warship").css("left","100px");
 		$("#submarine").css("left","200px");
@@ -93,9 +101,18 @@
 		$("#tugboat").css("left","400px");
 		$("#liferaft").css("left","480px");
 		
-		$(".piece").bind("mousedown",function(e){
+		var $pieces = $(".piece");
+		
+		$pieces.each(function(){
+			var $this = $(this), count = +($this.attr("rel")), $hitpeg = $this.find(".hitpeg");
+			for (var i=1; i<count; i++) {
+				$this.append($hitpeg.clone());
+			}
+		});
+		
+		$pieces.bind("mousedown",function(e){
 			if ($(this).hasClass("moveable") && !dragging) {
-				start_piece_drag(e);
+				start_piece_drag.call(this,e);
 			}
 		});
 
