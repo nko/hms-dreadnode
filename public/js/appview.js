1b06f6625d8eee149790cf7ec7724a18359e043a
Kyle Simpson <getify@gmail.com>
2010-08-29 01:38:04 -0500

    preparing for single-page app view

---
+++ public/js/appview.js
@@ -0,0 +1,11 @@
+(function(global,$){
+	var current_page_stack = ["index.html"];
+	var page_cache = {};
+
+
+	global.APPVIEW = {
+		next_page:function(){}
+		back_page:function(){}
+	};
+		  
+})(window,jQuery);
