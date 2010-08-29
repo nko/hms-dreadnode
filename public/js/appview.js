b6de08374eeb7700ef08814ecde070e56e3d33ef
Daniel Shaw <github@dshaw.com>
2010-08-29 13:40:44 -0400

    Yanking setup placeholder page from views

--- public/js/appview.js
+++ public/js/appview.js
@@ -1,6 +1,6 @@
 (function(global,$){
-  // pulled "gameselect.html" temporarily
-	var page_stack = ["index.html","login.html","gamesetup.html","gameboard.html","results.html"],
+  // pulled "gameselect.html" temporarily "gamesetup.html", too
+	var page_stack = ["index.html","login.html","gameboard.html","results.html"],
 		page_cache = {},
 		current_page = "index.html",
 		$index_content,
